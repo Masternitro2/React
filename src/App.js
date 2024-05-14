7501415bd4c4d18d1a7c5f9ab8011153e444fec3
Fernando Gonzalez <fg796041@gmail.com>
2024-05-14 08:49:25 -0600

    primer commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function MyButton() {
+  return (
+   
+   <center><a href="https://marketing4ecommerce.net/wp-content/uploads/2024/02/ias-generadoras-de-imagenes.jpg" button>
+      Ir
+    </a></center>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      
+     <center><img src="https://media.es.wired.com/photos/6501e7429fa9000811a95fe8/16:9/w_2560%2Cc_limit/Adobe%2520Firefly.jpeg" width="200" height="100"></img></center> 
+      <h1>Bienvenido a mi App</h1>
+      <br></br>
+      <p>Lorem</p>
+      <MyButton />
+    </div>
+  );
+}
